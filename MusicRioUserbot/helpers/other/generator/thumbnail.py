cebd9b8c45214b8b0b7ba49115b57189bc8eaba0
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-23 06:03:56 +0700

    Update thumbnail.py

--- MusicRioUserbot/helpers/other/generator/thumbnail.py
+++ MusicRioUserbot/helpers/other/generator/thumbnail.py
@@ -14,7 +14,7 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 
-async def gen_thumb(thumbnail, title, userid, ctitle):
+async def gen_thumb(thumbnail, title, id, ctitle):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
@@ -27,8 +27,8 @@ async def gen_thumb(thumbnail, title, userid, ctitle):
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
     image6 = image4.convert("RGBA")
-    Image.alpha_composite(image5, image6).save(f"search/temp{userid}.png")
-    img = Image.open(f"search/temp{videoid}.png")
+    Image.alpha_composite(image5, image6).save(f"search/temp{id}.png")
+    img = Image.open(f"search/temp{id}.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("MusicRioUserbot/helpers/other/choose/Roboto-Light.ttf", 55)
     font2 = ImageFont.truetype("MusicRioUserbot/helpers/other/choose/finalfont.ttf", 65)
