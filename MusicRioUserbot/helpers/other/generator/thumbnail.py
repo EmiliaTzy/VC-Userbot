178f9bb3b6b946adebb1d7f1fbfccdd285e0d472
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-08-23 01:33:47 +0700

    Update thumbnail.py

--- MusicRioUserbot/helpers/other/generator/thumbnail.py
+++ MusicRioUserbot/helpers/other/generator/thumbnail.py
@@ -18,17 +18,17 @@ async def gen_thumb(thumbnail, title, userid, ctitle):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
-                f = await aiofiles.open(f"search/thumb{userid}.png", mode="wb")
+                f = await aiofiles.open(f"search/thumb{videoid}.png", mode="wb")
                 await f.write(await resp.read())
                 await f.close()
-    image1 = Image.open(f"search/thumb{userid}.png")
+    image1 = Image.open(f"search/thumb{videoid}.png")
     image2 = Image.open(f"MusicRioUserbot/helpers/other/choose/rrc.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
     image6 = image4.convert("RGBA")
     Image.alpha_composite(image5, image6).save(f"search/temp{userid}.png")
-    img = Image.open(f"search/temp{userid}.png")
+    img = Image.open(f"search/temp{videoid}.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("MusicRioUserbot/helpers/other/choose/Roboto-Light.ttf", 55)
     font2 = ImageFont.truetype("MusicRioUserbot/helpers/other/choose/finalfont.ttf", 65)
@@ -48,8 +48,8 @@ async def gen_thumb(thumbnail, title, userid, ctitle):
         stroke_fill="black",
         font=font,
     )
-    img.save(f"search/final{userid}.png")
-    os.remove(f"search/temp{userid}.png")
-    os.remove(f"search/thumb{userid}.png")
-    final = f"search/final{userid}.png"
+    img.save(f"search/final{videoid}.png")
+    os.remove(f"search/temp{videoid}.png")
+    os.remove(f"search/thumb{videoid}.png")
+    final = f"search/final{videoid}.png"
     return final
